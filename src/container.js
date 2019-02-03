d02c6d7863e1b91800830811ddbf6c17a92b83ba
Josh Johnson <josh@joshuajohnson.co.uk>
2019-02-03 12:40:45 +0000

    Pass components through container

--- src/container.js
+++ src/container.js
@@ -1,18 +1,34 @@
 import { createContainer, asFunction, asValue } from 'awilix';
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import App from './components/App';
+import Layout from './components/Layout';
+import Header from './components/Header';
 import Navigation from './components/Navigation';
+import LayoutContainer from './components/LayoutContainer';
+import ContentSection from './components/ContentSection';
+import Code from './components/Code';
+import Logo from './components/Logo';
+import Footer from './components/Footer';
 
 const container = createContainer();
 
 container.register({
   React: asValue(React),
+  PropTypes: asValue(PropTypes),
 });
 
 container.register({
   App: asFunction(App),
+  Layout: asFunction(Layout),
+  Logo: asFunction(Logo),
+  Footer: asFunction(Footer),
+  Header: asFunction(Header),
   Navigation: asFunction(Navigation),
+  LayoutContainer: asFunction(LayoutContainer),
+  ContentSection: asFunction(ContentSection),
+  Code: asFunction(Code),
 });
 
 export default container;
